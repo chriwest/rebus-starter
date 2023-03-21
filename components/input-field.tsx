2c4dffe81ad0be6e5e3df34d0a20e3e54fcba89a
Christian Westgaard <Christian@Wests-Air.local>
2023-03-21 21:45:55 +0100

    onepager er ferdig, mangler spoersmal

---
+++ components/input-field.tsx
@@ -0,0 +1,35 @@
+// InputField.tsx
+import React, { useState } from 'react';
+import styles from '@/styles/Input.module.css'
+
+interface InputFieldProps {
+  correctValue: string;
+  onValidation: (isValid: boolean) => void;
+}
+
+const InputField: React.FC<InputFieldProps> = ({ correctValue, onValidation }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    onValidation(inputValue === correctValue);
+  };
+
+  return (
+    <form className={styles.container} onSubmit={handleSubmit}>
+      <input
+      className={styles.inputs}
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+      />
+      <button className={styles.submit} type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default InputField;
