631f8744c20dc3e39d9899adbb1c6d3df638b9a6
Christian Westgaard <Christian@Wests-Air.local>
2023-03-28 22:09:51 +0200

    litt finpuss og ekstra slack for validering

--- components/input-field.tsx
+++ components/input-field.tsx
@@ -1,14 +1,21 @@
 // InputField.tsx
-import React, { useState } from 'react';
-import styles from '@/styles/Input.module.css'
+import React, { useState } from "react";
+import styles from "../styles/input.module.css";
 
 interface InputFieldProps {
-  correctValue: string;
+  correctValue: string[];
   onValidation: (isValid: boolean) => void;
+  id?: string;
+  label?: string;
 }
 
-const InputField: React.FC<InputFieldProps> = ({ correctValue, onValidation }) => {
-  const [inputValue, setInputValue] = useState('');
+const InputField: React.FC<InputFieldProps> = ({
+  correctValue,
+  onValidation,
+  id,
+  label,
+}) => {
+  const [inputValue, setInputValue] = useState("");
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
@@ -16,18 +23,24 @@ const InputField: React.FC<InputFieldProps> = ({ correctValue, onValidation }) =
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    onValidation(inputValue === correctValue);
+    const isValid = correctValue.includes(inputValue.trim().toLowerCase());
+    onValidation(isValid);
+
   };
 
   return (
     <form className={styles.container} onSubmit={handleSubmit}>
+      <label htmlFor={id}>{label}</label>
       <input
-      className={styles.inputs}
+        id={id}
+        className={styles.inputs}
         type="text"
         value={inputValue}
         onChange={handleChange}
       />
-      <button className={styles.submit} type="submit">Bekreft</button>
+      <button className={styles.submit} type="submit">
+        Submit
+      </button>
     </form>
   );
 };
